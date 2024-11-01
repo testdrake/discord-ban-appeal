bd2688183b2418626eda70e0fdbcd7e9f2515f03
Horizon Development <111465344+testdrake@users.noreply.github.com>
2024-11-01 04:09:30 -0700

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,30 +1,28 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
-import {oauth} from"../App"
+import { oauth } from "../App";
 import { ReactComponent as DiscordLogo } from '../Images/Discord-Logo-White.svg';
 
 const crypto = require('crypto');
 
 class Home extends Component {
-
-
     render() {
         const url = oauth.generateAuthUrl({
             scope: ["identify", "email"],
-            state: crypto.randomBytes(16).toString("hex"), // Be aware that randomBytes is sync if no callback is provided
+            state: crypto.randomBytes(16).toString("hex"), // Random bytes for state
         });
         return (
             <Grid container alignItems={"center"} justify="center" direction="column">
                 <Grid item xs={12}>
-                    <Button 
-                        startIcon={<DiscordLogo style={{width: '25px', height: 'auto'}} />} 
-                        href={url} 
-                        size={"large"} 
+                    <Button
+                        startIcon={<DiscordLogo style={{ width: '25px', height: 'auto' }} />}
+                        href={url}
+                        size={"large"}
                         className={"button"}
-                        >
-                            <b>Login with Discord</b>
-                        </Button>
+                    >
+                        <b>Login with Discord</b>
+                    </Button>
                 </Grid>
             </Grid>
         );
